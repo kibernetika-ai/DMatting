a34f2bf775b2ffe94e59241e77574c08e4e6e658
Alexander Gunin <guninalexander@gmail.com>
2018-12-26 14:05:23 +0300

    figure out

--- model/matting.py
+++ model/matting.py
@@ -6,24 +6,30 @@ import tensorflow as tf
 import logging
 
 
-def unpool(pool,
-           ind,
-           stride=[1, 2, 2, 1],
-           scope='unpool'):
-    with tf.variable_scope(scope):
+def unpool(pool, ind, ksize=[1, 2, 2, 1], name=None):
+    with tf.variable_scope('name') as scope:
         input_shape = tf.shape(pool)
-        output_shape = [input_shape[0], input_shape[1] * stride[1], input_shape[2] * stride[2], input_shape[3]]
+        output_shape = [input_shape[0], input_shape[1] * ksize[1], input_shape[2] * ksize[2], input_shape[3]]
 
-        flat_input_size = tf.reduce_prod(input_shape)
-        flat_output_shape = [output_shape[0], output_shape[1] * output_shape[2] * output_shape[3]]
+        flat_input_size = tf.cumprod(input_shape)[-1]
+        flat_output_shape = tf.stack([output_shape[0], output_shape[1] * output_shape[2] * output_shape[3]])
 
-        pool_ = tf.reshape(pool, [flat_input_size])
-        ind_ = tf.reshape(ind, [flat_input_size, 1])
+        pool_ = tf.reshape(pool, tf.stack([flat_input_size]))
+        batch_range = tf.reshape(tf.range(tf.cast(output_shape[0], tf.int64), dtype=ind.dtype),
+                                 shape=tf.stack([input_shape[0], 1, 1, 1]))
+        b = tf.ones_like(ind) * batch_range
+        b = tf.reshape(b, tf.stack([flat_input_size, 1]))
+        ind_ = tf.reshape(ind, tf.stack([flat_input_size, 1]))
+        ind_ = ind_ - b * tf.cast(flat_output_shape[1], tf.int64)
+        ind_ = tf.concat([b, ind_], 1)
 
-        ret = tf.scatter_nd(ind_, pool_, shape=[tf.cast(flat_output_shape[0], tf.int64)*tf.cast(flat_output_shape[1], tf.int64)])
-        ret = tf.reshape(ret, output_shape)
-        #ret.set_shape(output_shape)
-        return ret
+        ret = tf.scatter_nd(ind_, pool_, shape=tf.cast(flat_output_shape, tf.int64))
+        ret = tf.reshape(ret, tf.stack(output_shape))
+
+        set_input_shape = pool.get_shape()
+        set_output_shape = [set_input_shape[0], set_input_shape[1] * ksize[1], set_input_shape[2] * ksize[2], set_input_shape[3]]
+        ret.set_shape(set_output_shape)
+    return ret
 
 
 def _matting_model_fn(features, labels, mode, params=None, config=None, model_dir=None):

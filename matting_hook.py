74eb82d51cdb4d84c406999ad7298418d42c83ab
Alexander Gunin <guninalexander@gmail.com>
2018-12-29 00:24:34 +0300

    dump

--- matting_hook.py
+++ matting_hook.py
@@ -60,7 +60,7 @@ def preprocess(inputs, ctx):
         logging.warning('Mask shape is {}'.format(np_mask.shape))
         np_mask = np_mask[:,:,0]
     #np_mask[np.less(np_mask,128)]=0
-    np_mask[np.logical_and(np_mask>0, np_mask<230)]=0
+    np_mask[np.logical_and(np_mask>0, np_mask<200)]=0
     np_mask[np.greater(np_mask,200)]=255
     np_mask[np.less(np_mask,255)]=0
     ctx.np_mask = np_mask

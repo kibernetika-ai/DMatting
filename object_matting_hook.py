766b2a96564d44cabedf3d1e52a39309e40d8708
Alexander Gunin <guninalexander@gmail.com>
2019-01-10 21:57:26 +0300

    use max 1024

--- object_matting_hook.py
+++ object_matting_hook.py
@@ -5,8 +5,8 @@ import numpy as np
 from PIL import Image
 from PIL import ImageFilter
 from ml_serving.utils import helpers
-import cv2 as cv
 LOG = logging.getLogger(__name__)
+import math
 
 
 def init_hook(**params):
@@ -39,11 +39,10 @@ def preprocess(inputs, ctx):
             ratio = h/1024.0
 
     if ratio>1:
-        image = ctx.original_image.resize((w/ratio,h/ratio))
+        ctx.image = ctx.original_image.resize((w/ratio,h/ratio))
     else:
-        image = ctx.original_image
-    ctx.process_np_image = np.array(image)
-    ctx.original_np_image = np.array(ctx.original_image)
+        ctx.image = ctx.original_image
+    ctx.np_image = np.array(ctx.image)
     ctx.area_threshold = int(inputs.get('area_threshold', 0))
     ctx.max_objects = int(inputs.get('max_objects', 100))
     ctx.pixel_threshold = float(inputs.get('pixel_threshold', 0.5))
@@ -52,7 +51,7 @@ def preprocess(inputs, ctx):
     ctx.blur_radius = int(inputs.get('blur_radius', 2))
     ctx.interpolation = interploations[inputs.get('interpolation', ['BILINEAR'])[0].decode("utf-8")]#NEAREST,BICUBIC,BILINEAR
     ctx.matting = inputs.get('matting', ['DEFAULT'])[0].decode("utf-8")#DEFAULT,KNN,NONE
-    return {'inputs': [ctx.process_np_image]}
+    return {'inputs': [ctx.np_image]}
 
 
 def kibernetika_matte(img, trimap,pixel_threshold,out_put):
@@ -75,12 +74,12 @@ def postprocess(outputs, ctx):
 
     if num_detection < 1:
         return return_original()
-    process_width = ctx.process_np_image.shape[1]
-    process_height = ctx.process_np_image.shape[0]
 
-    image_area = process_width * process_height
+    width = ctx.np_image.size[0]
+    height = ctx.np_image.size[1]
+    image_area = width * height
     detection_boxes = outputs["detection_boxes"][0][:num_detection]
-    detection_boxes = detection_boxes * [process_height, process_width, process_height, process_width]
+    detection_boxes = detection_boxes * [height, width, height, width]
     detection_boxes = detection_boxes.astype(np.int32)
     detection_classes = outputs["detection_classes"][0][:num_detection]
     detection_masks = outputs["detection_masks"][0][:num_detection]
@@ -89,13 +88,15 @@ def postprocess(outputs, ctx):
     for i in range(num_detection):
         if int(detection_classes[i]) not in ctx.object_classes:
             continue
+        mask_image = Image.fromarray(detection_masks[i])
         box = detection_boxes[i]
-        mask_image = cv.resize(detection_masks[i],(box[3] - box[1], box[2] - box[0]), interpolation=cv.INTER_LINEAR)
+        mask_image = mask_image.resize((box[3] - box[1], box[2] - box[0]), ctx.interpolation)
         left = max(0,box[1]-50)
-        right = min(process_width,box[3]+50)
+        right = min(ctx.np_image.shape[1],box[3]+50)
         upper = max(0,box[0]-50)
-        lower = min(process_height,box[2]+50)
-        box_mask = np.pad(mask_image, ((box[0]-upper, lower-box[2]), (box[1]-left, right - box[3])), 'constant')
+        lower = min(ctx.np_image.shape[0],box[2]+50)
+        box_mask = np.array(mask_image)
+        box_mask = np.pad(box_mask, ((box[0]-upper, lower-box[2]), (box[1]-left, right - box[3])), 'constant')
         area = int(np.sum(np.greater_equal(box_mask, ctx.pixel_threshold).astype(np.int32)))
         if area * 100 / image_area < ctx.area_threshold:
             continue
@@ -104,30 +105,31 @@ def postprocess(outputs, ctx):
     if len(masks) < 1:
         return return_original()
     masks = sorted(masks, key=lambda row: -row[0])
-    total_mask = np.zeros((process_height, process_width), np.float32)
+    total_mask = np.zeros((height, width), np.float32)
     for i in range(min(len(masks), ctx.max_objects)):
         pre_mask = masks[i][1]
         box = masks[i][2]
         left = max(0,box[1])
-        right = min(process_width,box[3])
+        right = min(ctx.np_image.shape[1],box[3])
         upper = max(0,box[0])
-        lower = min(process_height,box[2])
+        lower = min(ctx.np_image.shape[0],box[2])
         if ctx.matting == 'DEFAULT':
             pre_mask[np.less(pre_mask, ctx.pixel_threshold)] = 0
         elif ctx.matting == 'Kibernetika':
-            pre_mask = kibernetika_matte(ctx.process_np_image[upper:lower,left:right,:],np.uint8(pre_mask*255),ctx.pixel_threshold,0)
+            pre_mask = kibernetika_matte(ctx.np_image[upper:lower,left:right,:],np.uint8(pre_mask*255),ctx.pixel_threshold,0)
         elif ctx.matting == 'Testing':
-            pre_mask = kibernetika_matte(ctx.process_np_image[upper:lower,left:right,:],np.uint8(pre_mask*255),ctx.pixel_threshold,1)
+            pre_mask = kibernetika_matte(ctx.np_image[upper:lower,left:right,:],np.uint8(pre_mask*255),ctx.pixel_threshold,1)
             outputs['output'] = pre_mask
             return outputs
-        box_mask = np.pad(pre_mask, ((upper, ctx.process_height - lower), (left, ctx.process_width - right)), 'constant')
+        box_mask = np.pad(pre_mask, ((upper, height - lower), (left, width - right)), 'constant')
         total_mask = np.maximum(total_mask,box_mask)
-
-    if total_mask.shape != ctx.original_np_image.shape:
-        total_mask = cv.resize(total_mask,(ctx.original_np_image.shape[1],ctx.original_np_image.shape[0]),interpolation=cv.INTER_LINEAR)
-
+    if total_mask.shape[0] != ctx.original_image.size[1] or total_mask.shape[1] != ctx.original_image.size[0]:
+        total_mask = Image.fromarray(np.uint8(total_mask*255))
+        total_mask = total_mask.resize((ctx.original_image.size[0],ctx.original_image.size[1]))
     if ctx.effect == 'Remove background':
-        image = ctx.original_np_image.astype(np.float32)
+        original_np_image = np.array(ctx.original_image)
+        image = original_np_image.astype(np.float32)
+        #image = np.expand_dims(total_mask,2)*image
         total_mask = np.uint8(total_mask*255)
         image = np.dstack((image, total_mask))
         image = Image.fromarray(np.uint8(image))
@@ -135,14 +137,15 @@ def postprocess(outputs, ctx):
         total_mask = total_mask*255
         image = Image.fromarray(np.uint8(total_mask))
     else:
-        image = ctx.original_np_image.astype(np.float32)
+        original_np_image = np.array(ctx.original_image)
+        image = original_np_image.astype(np.float32)
         mask = np.expand_dims(total_mask,2)
         foreground = mask*image
         radius = min(max(ctx.blur_radius,2),10)
         if ctx.effect == 'Grey':
-            background = rgb2gray(ctx.original_np_image)
+            background = rgb2gray(ctx.np_image)
         else:
-            background = ctx.original_image.filter(ImageFilter.GaussianBlur(radius=radius))
+            background = ctx.image.filter(ImageFilter.GaussianBlur(radius=radius))
         background = (1.0-mask)*np.array(background,dtype=np.float32)
         image = foreground+background
         image = Image.fromarray(np.uint8(image))
